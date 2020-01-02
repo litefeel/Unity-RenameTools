68c773640ad1427a9f07f851d343d9a7069dffdf
litefeel <litefeel@gmail.com>
2020-01-03 00:20:06 +0800

    Update shortcut and menu item

--- Editor/RenameToolsMenu.cs
+++ Editor/RenameToolsMenu.cs
@@ -1,19 +1,12 @@
 using UnityEditor;
-#if UNITY_2019_1_OR_NEWER
-using UnityEditor.ShortcutManagement;
-using UnityEngine;
-#endif
 
 namespace litefeel.RenameTools
 {
     public static class RenameToolsMenu
     {
-#if UNITY_2019_1_OR_NEWER
-        private const string WindowMenuPath = "Window/LiteFeel/Rename Tools";
-#else
-        private const string WindowMenuPath = "Window/LiteFeel/Rename Tools/Align Tools %#R";
-#endif
-        // Creation of window
+
+        private const string WindowMenuPath = "Window/LiteFeel/Rename Tools #F2";
+
         [MenuItem(WindowMenuPath)]
         private static void RenameToolsWindows()
         {
